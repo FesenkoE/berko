ac3c02e75e421ff2f1bd5024be7e2745e55cfbcc
Fedor-Ivanov <fedorhim@gmail.com>
2019-09-13 21:36:37 +0300

    13.09

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,12 @@
-alert("hi");
+$(document).ready(function () {
+	
+	$('.featured__slider').slick({
+		dots: false,
+		infinite: true,
+		speed: 400,
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		arrows: true
+	});
+
+});
